d8fe47c9611360056f5e634db4f202758b71ea2d
Damian Niemczyk <djniemczyk7@googlemail.com>
2016-02-22 23:39:23 +0100

    Improve triple naming

--- src/tic_tac_toe_clj/core.clj
+++ src/tic_tac_toe_clj/core.clj
@@ -11,12 +11,16 @@
 (defn triples
   "Splits the board into a collection of winnig triples"
   [board]
-  (concat
-   (partition-all 3 board)
-   (list
-    (take-nth 3 board)
-    (take-nth 3 (drop 1 board))
-    (take-nth 3 (drop 2 board)))))
+  (letfn [(horizontal-triples     [x] (partition-all 3 x))
+          (first-vertical-triple  [x] (take-nth 3 x))
+          (second-vertical-triple [x] (take-nth 3 (drop 1 x)))
+          (third-vertical-triple  [x] (take-nth 3 (drop 2 x)))]
+    (concat
+     (horizontal-triples board)
+     (list
+      (first-vertical-triple  board)
+      (second-vertical-triple board)
+      (third-vertical-triple  board)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
