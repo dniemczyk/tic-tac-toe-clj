a63d8c84ed3ddb5f04bbbd9c9d8d7245e873dc99
Damian Niemczyk <djniemczyk7@googlemail.com>
2016-03-26 18:43:15 +0100

    Start with some idea of a goal
    for a closing move

--- src/tic_tac_toe_clj/computer.clj
+++ src/tic_tac_toe_clj/computer.clj
@@ -1,10 +1,13 @@
 (ns tic-tac-toe-clj.computer
   "Decision module for a computer player implementation"
   (:refer-clojure :exclude [==])
-  (:require [clojure.core.logic :refer :all]))
+  (:require [clojure.core.logic :refer :all]
+            [clojure.set :as set]))
 
-(defn test-find
-  [x]
-  (run* [q]
-    (== q x)))
-
+(defn closing-move
+  [occupied-spaces]
+  (let [board (set (range 1 10))
+        free-spaces (vec (set/difference board
+                                         (set occupied-spaces)))]
+    (run* [next-move]
+      (membero next-move free-spaces))))
