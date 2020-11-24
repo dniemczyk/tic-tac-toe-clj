0e7a91d5ff71333312600bb29c732404a6a884b8
Damian Niemczyk-Brunnbauer <djniemczyk7@googlemail.com>
2020-11-24 19:43:57 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.logic "0.8.10"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.logic "1.0.0"]
                  [colorize "0.1.1" :exclusions [org.clojure/clojure]]]
   :main ^:skip-aot tic-tac-toe-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.2"]]}})
+             :dev {:dependencies [[midje "1.9.9"]]
+                   :plugins [[lein-midje "3.2.2"]]}})
